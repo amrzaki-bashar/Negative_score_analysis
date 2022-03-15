169258402cdbe4e4ee736b16e4eff186bff28235
amrzaki-bashar <amr.zaki@basharsofr.com>
2022-03-15 16:53:06 -0400

    (inital push)

---
+++ comparing.py
@@ -0,0 +1,49 @@
+
+import requests
+import json
+import statistics
+import pandas as pd
+from tqdm import tqdm 
+
+search_query_list = []
+with open('assets\\top_100_search_archive_15_3_2022.json') as f:
+	data = json.load(f)
+	for i in data['aggregations']['search_query']['buckets']:
+		search_query_list.append(i['key'])
+print(search_query_list)
+headers = {'Content-Type': 'application/json'}
+
+result = {}
+for search_query in search_query_list:
+    default = {}
+    with open('default\\'+search_query+'.json', 'r',encoding='utf-8') as file:
+        data = json.load(file)
+        for i in data['data']:
+            default[i["id"]] = i["meta"]["score"]
+
+    new_mapping = {}
+    with open('new_mapping\\'+search_query+'.json', 'r',encoding='utf-8') as file:
+        data = json.load(file)
+        for i in data['data']:
+            new_mapping[i["id"]] = i["meta"]["score"]
+
+    #get difference between them both
+    difference = {}
+    for key,value in default.items():
+        if(key in new_mapping.keys()):
+            difference[key] = default[key] - new_mapping[key]
+    
+
+    
+    same_results = len(difference)
+    standard_deviation = -1
+    if(same_results>1):
+        standard_deviation = statistics.stdev(difference.values())
+    result[search_query] = {
+        "standard_deviation":standard_deviation,
+        "number of same results": same_results}
+
+df = pd.DataFrame(result)
+df.T.to_csv('result.csv')
+
+print()
---
+++ query.py
@@ -0,0 +1,44 @@
+
+import requests
+import json
+from tqdm import tqdm 
+
+search_query_list = []
+with open('assets\\top_100_search_archive_15_3_2022.json') as f:
+	data = json.load(f)
+	for i in data['aggregations']['search_query']['buckets']:
+		search_query_list.append(i['key'])
+print(search_query_list)
+headers = {'Content-Type': 'application/json'}
+
+url = "http://192.168.2.61:5001/jobs/search"
+
+for search_query in tqdm(search_query_list):
+	payload = json.dumps({
+		"latitude": "0.0",
+		"longitude": "0.0",
+		"pageSize": 100,
+		"query": search_query,
+		"searchFilters": {},
+		"startIndex": 0
+	})
+	response = requests.request("POST", url, headers=headers, data=payload)
+	default_out = json.loads(response.text)
+	with open('default\\'+search_query+'.json', 'w',encoding='utf-8') as outfile:
+		json.dump(default_out, outfile, sort_keys = True, indent = 4,ensure_ascii = False)
+
+	payload = json.dumps({
+		"latitude": "0.0",
+		"longitude": "0.0",
+		"pageSize": 100,
+		"query": search_query,
+		"searchFilters": {},
+		"startIndex": 0,
+		"index_name":"jobs-2"
+	})
+	response = requests.request("POST", url, headers=headers, data=payload)
+	new_mapping_out = json.loads(response.text)
+	with open('new_mapping\\'+search_query+'.json', 'w',encoding='utf-8') as outfile:
+		json.dump(new_mapping_out, outfile, sort_keys = True, indent = 4,ensure_ascii = False)
+
+print(response.text)
